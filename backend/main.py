95925e38d90a53d07d3a4c7d3f7a6d67d5148bdd
Brad Huang <bradhuang1999@yahoo.com>
2020-01-30 13:21:34 -0500

    Backend helloworld update

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,17 +9,17 @@ def hello():
 
 @app.route("/courses")
 def courses():
-	return "CS 101, CS 102, CS348"
+	return "CS 101, CS 102, CS 348"
 
 @app.route("/course-query")
 def courseQuery():
 	degree = request.args.get("degree", default = "", type = str)
 	if degree == "CS":
-		return "CS 101, CS 102, CS348"
+		return "CS 101, CS 102, CS 348"
 	elif degree == "SE":
-		return "SE 101, SE 102, CS348"
+		return "SE 101, SE 102, CS 348"
+	else:
+		return "Try degree=CS or degree=SE"
 
 if __name__ == "__main__":
 	app.run()
-
-
