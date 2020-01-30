778034c300536f8ae55cfc280f988c813b1d9a02
Brad Huang <bradhuang1999@yahoo.com>
2020-01-30 15:49:20 -0500

    Hello World app

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,33 @@
 from flask import Flask
 from flask import request
+import psycopg2
 
 app = Flask(__name__)
 
+connection = psycopg2.connect(
+	database = "postgres",
+	user = "postgres",
+	password = "password",
+	host = "/cloudsql/uwaterloo-cs348:us-central1:cs348-db"
+)
+
 @app.route("/")
-def hello():	
+def hello():
 	return "Hello World!"
 
 @app.route("/courses")
 def courses():
-	return "CS 101, CS 102, CS 348"
+	return "Current courses are CS 101, CS 102, CS 348"
+
+@app.route("/degrees")
+def degrees():
+	cur = connection.cursor()
+	cur.execute("""SELECT degree FROM degree""")
+	rows = cur.fetchall()
+
+	degrees = "\n".join(map(lambda d: d[0], rows))
+
+	return f"Current degrees are:\n{degrees}"
 
 @app.route("/course-query")
 def courseQuery():
