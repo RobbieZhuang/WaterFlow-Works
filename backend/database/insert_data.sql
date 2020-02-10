97d65c65619e9fc963857a4fd68ce91781f0dade
Jingfei Peng <36685292+JingfeiPeng@users.noreply.github.com>
2020-02-10 00:37:59 -0500

    fixed err in insert_data.sql

--- backend/database/insert_data.sql
+++ backend/database/insert_data.sql
@@ -1,26 +1,26 @@
 INSERT INTO degree (name) VALUES ('Computer Science');
 
-INSERT INTO faculty (name) VALUES ("Math");
-INSERT INTO faculty (name) VALUES ("Engineering");
-INSERT INTO faculty (name) VALUES ("Art");
+INSERT INTO faculty (name) VALUES ('Math');
+INSERT INTO faculty (name) VALUES ('Engineering');
+INSERT INTO faculty (name) VALUES ('Art');
 
-INSERT INTO subject (name) VALUES ("CS", 1);
+INSERT INTO subject (name,faculty_id) VALUES ('CS', 1);
 
 -- insert cs246 - 1
 INSERT INTO course (name, code, subject_id ) 
-VALUES ("Object-Oriented Software Development", "246", 1);
+VALUES ('Object-Oriented Software Development', '246', 1);
 -- insert cs136 - 2
 INSERT INTO course (name, code, subject_id ) 
-VALUES ("Elementary Algorithm Design and Data Abstraction", "136", 1);
+VALUES ('Elementary Algorithm Design and Data Abstraction', '136', 1);
 -- insert cs135 - 3
 INSERT INTO course (name, code, subject_id ) 
-VALUES ("Functional Programming", "135", 1);
+VALUES ('Functional Programming', '135', 1);
 -- insert cs116 - 4
 INSERT INTO course (name, code, subject_id ) 
-VALUES ("Introduction to Computer Science 2", "116", 1);
+VALUES ('Introduction to Computer Science 2', '116', 1);
 -- insert cs115 - 5
 INSERT INTO course (name, code, subject_id ) 
-VALUES ("Introduction to Computer Science 1", "115", 1);
+VALUES ('Introduction to Computer Science 1', '115', 1);
 
 
 
