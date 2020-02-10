8daa2409b0bb2dead5545723d3e4d003a2503141
Jingfei Peng <36685292+JingfeiPeng@users.noreply.github.com>
2020-02-10 01:55:28 -0500

    added an api to validate if input course exist in our table

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS
 import psycopg2
+from psycopg2 import sql
 import json
 
 app = Flask(__name__)
@@ -41,6 +42,36 @@ def hello():
 @app.route("/courses")
 def courses():
 	return json.dumps(['CS 101','CS 102', 'CS 348'])
+
+# This api validates if an input course is valid in our table.
+@app.route("/validateCourse") # param name course: http://127.0.0.1:5000/validateCourse?course=cs348
+def validateCourseName():
+	course = request.args.get('course')
+
+	# get subject and code from course
+	between = 0
+	for i in range(len(course)):
+		if course[i].isdigit():
+			between = i
+			break
+	subjectPart = course[0:i]
+	codePart = course[i:]
+
+	# get subject id
+	cur = connection.cursor()
+	cur.execute("SELECT subject_id FROM subject WHERE UPPER(name) = UPPER('%s')" % subjectPart)
+	rows = cur.fetchall()
+	# if subject input is invalide like 'as' instead of 'cs'
+	if len(rows) == 0: 
+		return json.dumps([])
+	subjectId = list(map(lambda d: d[0], rows))[0]
+
+	# get course data
+	cur.execute(sql.SQL("SELECT name, subject_id, code FROM course WHERE subject_id = %s AND code = %s"), [subjectId, codePart])
+	rows = cur.fetchall()
+	res = list(map(lambda d: [d[0], subjectPart, d[2]], rows))
+	return json.dumps(res)
+
 
 @app.route("/course-path", methods=['POST'])
 def coursePath():
