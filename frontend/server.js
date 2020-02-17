babe7f948afa53d21f41e67fd2e5ce1009a73bd3
Jingfei Peng <36685292+JingfeiPeng@users.noreply.github.com>
2020-02-17 03:32:42 -0500

    deploy frontend to live site

---
+++ frontend/server.js
@@ -0,0 +1,23 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/frontend'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+});
+
+// app.get('*', (req, res) => {
+//     res.sendFile(`./front-end/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+// });
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
+    // "start": "ng serve --port=4201",
+
+
